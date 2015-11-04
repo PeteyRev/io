0ad097e7fb3c0c6e7c89b77665751421b4d0dd4f
Peter Arevalo <peteywiggin@gmail.com>
2015-11-04 15:16:12 -0600

    finished Towers of Hanoi

--- Towers/main.js
+++ Towers/main.js
@@ -1,13 +1,29 @@
-$(document).ready(function(){
-	var $lastChild = null;
-	$('[data-stack]').click(function(){
-		if ($lastChild === null) {
-		 $lastChild = $(this).children().last().detach();
-		} else {
-		$(this).append($lastChild);
-	    $lastChild = null;
-		}	   
-	});
+$(document).ready(function() {
+    var $selected = null;
+    $('[data-stack]').click(function() {
+        if ($selected === null) {
+            $selected = $(this).children().last().detach();
+        } else {
+            if (isLegal($(this))) {
+                $(this).append($selected);
+				checkWin();
+                $selected = null;
+            } 
+        }
+    });
 
-
+    function isLegal($stack) {
+		if (parseInt($stack.children().last().attr('data-block')) > parseInt($selected.attr('data-block')) || $stack.children().length === 0) {
+			return true;
+		} 
+		return false;
+    }
+	
+	function checkWin() {
+		if ($('[data-stack="2"]').children().length === 4 || $('[data-stack="3"]').children().length === 4){
+			$('#announce-game-won').append('<h1>YOU WIN!</h1>');
+		}
+	}
 });
+
+// parseInt - not comparing number, only strings
